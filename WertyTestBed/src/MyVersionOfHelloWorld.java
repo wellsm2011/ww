98c866328c5751aedc1cb54a07eb7ba6fcffd08f
Andrew Binns <abinns2011@my.fit.edu>
2015-06-06 19:22:45 -0400

    Cleaned and formatted.

--- WertyTestBed/src/MyVersionOfHelloWorld.java
+++ WertyTestBed/src/MyVersionOfHelloWorld.java
@@ -1,6 +1,9 @@
-public class MyVersionOfHelloWorld {
-	public static void main(String... Cheese) {
-		for (int i = 1; i < 10; i += 2) {
+public class MyVersionOfHelloWorld
+{
+	public static void main(String... Cheese)
+	{
+		for (int i = 1; i < 10; i += 2)
+		{
 			for (int j = 0; j < 9 - i / 2; j++)
 				System.out.print(" ");
 
@@ -10,7 +13,8 @@ public class MyVersionOfHelloWorld {
 			System.out.print("\n");
 		}
 
-		for (int i = 7; i > 0; i -= 2) {
+		for (int i = 7; i > 0; i -= 2)
+		{
 			for (int j = 0; j < 9 - i / 2; j++)
 				System.out.print(" ");
 
