8377f1313380a13d0baa1eda2691d13b5cec3c89
Andrew Binns <abinns2011@my.fit.edu>
2015-06-06 07:47:32 -0400

    More config tweaks

--- TestBed/src/config/Item.java
+++ TestBed/src/config/Item.java
@@ -9,7 +9,7 @@ public class Item
 	public Item(String name, JSONObject data)
 	{
 		for (String cur : data.keySet())
-			U.p(name + " - " + cur + ":" + data.getString(cur));
+			U.p(name + " - " + cur + ":" + data.get(cur).toString());
 	}
 
 }
